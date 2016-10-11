f6dedc1cef01c5441f3104e3768b8d08a90ba04b
Kevin Baxter <kbax6342@gmail.com>
2016-10-11 09:10:05 -0400

    live website

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-$(window).swith(function(){
-  if(open > 1){
-    console.log"I have not used this in a while";
-  }else{
-    console.log"I still have not used jquery in a while";
-  }
-});
+var app = angular.module('action',[]);
